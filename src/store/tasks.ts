9dd466bd610c6b29dac49aab798f486954ce12fe
Agywa <91954956+Agywa@users.noreply.github.com>
2022-01-20 20:10:26 +0300

    11/1/22

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -1,10 +1,18 @@
+export const addSalary = (salary: number, bonus: number) => salary + bonus
+export const fallSalary = (salary: number, minus: number) => salary - minus
+export const multSalary = (salary: number, coefficient: number) => salary * coefficient
+export const divSalary = (salary: number, coefficient: number) => salary * coefficient
 
-export const addSalary = (salary: number, bonus:number) => salary + bonus
-export const fallSalary = (salary: number, minus:number) => salary - minus
-export const multSalary = (salary: number, coefficient:number) => salary * coefficient
-export const divSalary = (salary: number, coefficient:number) => salary * coefficient
+// 1. В параметрах есть salary (state)
+// 2.  Тип действия в названии (type of action/ action type )
+// 3. Доп значения дял каждого типа действия
+
+type Actiontype = {
+    type: string
+    payload: number
+}
 
 
+export const salaryReducer = (salary: number, action: Actiontype) => {
 
-
-
+}
